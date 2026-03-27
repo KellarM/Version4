3db5dc78a906666c95363bbe2853875ba2660d07
michael kellar <kellar_em@yahoo.ca>
2026-03-27 08:42:01 +0000

    File changes

--- src/components/game/PayoutTable.jsx
+++ src/components/game/PayoutTable.jsx
@@ -3,31 +3,6 @@ import { FIXED_HANDS, SUITS, HAND_RANK_PAYOUTS, RB_TABLE } from '@/lib/gameEngin
 export default function PayoutTable() {
   return (
     <div className="flex flex-col gap-3 text-xs">
-      {/* Carded Hand Payouts */}
-      <div className="border border-yellow-700/40 rounded-xl p-3 bg-black/30">
-        <div className="text-yellow-400 text-xs font-bold tracking-wider uppercase mb-2 text-center">Carded Hand Payouts</div>
-        <table className="w-full">
-          <thead>
-            <tr className="border-b border-yellow-700/30">
-              <th className="text-left text-yellow-400/70 pb-1 font-semibold">Hand</th>
-              <th className="text-right text-yellow-400/70 pb-1 font-semibold">Payout</th>
-            </tr>
-          </thead>
-          <tbody>
-            {FIXED_HANDS.map(hand => (
-              <tr key={hand.id} className="border-b border-green-900/30">
-                <td className="py-0.5">
-                  <span className="text-green-300/80">
-                    {hand.cards.map(c => `${c.rank}${SUITS[c.suit]}`).join('/')}
-                  </span>
-                </td>
-                <td className="text-right text-yellow-300 font-bold">{hand.payout}:1</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
       {/* Hand Rank Payouts */}
       <div className="border border-yellow-700/40 rounded-xl p-3 bg-black/30">
         <div className="text-yellow-400 text-xs font-bold tracking-wider uppercase mb-2 text-center">Rank Position Payouts</div>
