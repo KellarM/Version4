830b89f5791190ce9896449974351540b29bab7b
michael kellar <kellar_em@yahoo.ca>
2026-04-27 21:45:46 +0000

    File changes

--- src/lib/workerBridge.js
+++ src/lib/workerBridge.js
@@ -1,1 +1,17 @@
 // Worker Bridge
+
+export function runBetAuditWithAbort(params, onProgress) {
+  return { promise: Promise.resolve({ success: true }), abort: () => {} };
+}
+
+export function runMicroscopeWithAbort(params) {
+  return { promise: Promise.resolve({ success: true }), abort: () => {} };
+}
+
+export function runExportWithAbort(params, onChunk, onProgress) {
+  return { promise: Promise.resolve({ total: 0 }), abort: () => {} };
+}
+
+export function resetPersistentWorker() {
+  // placeholder
+}
